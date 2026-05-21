c008e56d2b2623832f3e87287641217dd36474ba
Egor Doronin <108686494+Xlamid@users.noreply.github.com>
2026-05-21 20:46:30 +0600

    Добавил класс Hello для вывода приветствия
    * fix: gitignore
    
    * add: Hello
    
    * fix: update message in Hello.java
    
    * add: second print line

---
+++ src/main/java/git/xlamid/Hello.java
@@ -0,0 +1,9 @@
+package git.xlamid;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Update message in Hello.java");
+        System.out.println("End of demo.");
+    }
+}
