31ef25714a12071204dd755f179d72ac049c127a
Xlamid <doronin52.com@gmail.com>
2026-05-21 20:42:55 +0600

    fix: update message in Hello.java

--- src/main/java/git/xlamid/Hello.java
+++ src/main/java/git/xlamid/Hello.java
@@ -3,6 +3,6 @@ package git.xlamid;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Git!");
+        System.out.println("Update message in Hello.java");
     }
 }
